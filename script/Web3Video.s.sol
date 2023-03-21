243efd4c3d6622272825f11562e49293236e3538
ihossain-6 <ismumhossain@gmail.com>
2023-03-21 22:37:56 +0600

    Update name

--- script/Web3Video.s.sol
+++ script/Web3Video.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "../src/Web3Video.sol";
 
-contract DeployNFT is Script {
+contract DeployWeb3Video is Script {
     function run() public {
         vm.startBroadcast();
         new Web3Video();
